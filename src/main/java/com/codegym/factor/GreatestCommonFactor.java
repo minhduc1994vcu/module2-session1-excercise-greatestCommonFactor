ba0e716cdb700412f2123a51fdeabd86f9b6e07b
minhduc <minhduc1994vcu@gmail.com>
2019-03-30 17:12:05 +0700

    input 2 number

--- src/main/java/com/codegym/factor/GreatestCommonFactor.java
+++ src/main/java/com/codegym/factor/GreatestCommonFactor.java
@@ -1,4 +1,13 @@
 package com.codegym.factor;
 
+import java.util.Scanner;
+
 public class GreatestCommonFactor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the first number: ");
+        int number1 = Math.abs(scanner.nextInt());
+        System.out.println("enter the second number: ");
+        int number2 = Math.abs(scanner.nextInt());
+    }
 }
